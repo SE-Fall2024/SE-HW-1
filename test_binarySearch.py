624e9bb222349e0c692e61d8f405477bc86cfab7
SAI SUMANTH <42322472+SaisumanthT@users.noreply.github.com>
2024-08-27 00:10:01 +0000

    Step-9: Adding test file

---
+++ test_binarySearch.py
@@ -0,0 +1,10 @@
+# test_binarySearch.py
+from Binary_Search import binary_search
+
+def test_binarySeach_pass():
+    
+    assert binary_search([1,4,6,8,10],8) == True  
+
+def test_binarySeach_fail():
+   
+    assert binary_search([1,4,6,8,10],14) == True  
