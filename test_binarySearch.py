fca4a785fb50bd628252b8046d23280d26934658
Rajasribhagam23 <rajasribhagam23@gmail.com>
2024-08-27 19:48:50 -0400

    Updated code

--- test_binarySearch.py
+++ test_binarySearch.py
@@ -3,7 +3,7 @@ from Binary_Search import binary_search
 
 def test_binarySeach_pass():
     
-    assert binary_search([1,4,6,8,10],81) == True  
+    assert binary_search([1,4,6,8,10],8) == True  
 
 def test_binarySeach_fail():
    
